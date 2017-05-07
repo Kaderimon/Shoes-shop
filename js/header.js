7df94e1e27050a0e8769c19ddc0fc39a27d2b4ac
Kaderimon <schweppes2006@online.ua>
2017-05-07 23:21:50 +0300

    finished index & begun category page

--- js/header.js
+++ js/header.js
@@ -2,28 +2,34 @@
 
 	document.addEventListener("DOMContentLoaded", function() {
 		document.body.addEventListener("click", function(e) {
-			let targetClassName = e.target.className;
-			switch (true) {
-				case targetClassName.search('logo__text')>-1:
+			switch (e.target.className) {
+				case 'logo__text':
 					location.href = '#';
 					break;
-				case targetClassName.search('even')>-1:
-					location.href = '#';//category listing
+				case 'search-button':
+					let formShow = document.querySelector('.search-form').style.display = 'table';
+					e.target.style.display = 'none';
+					document.querySelector('.search-form__input').focus();
 				break;
-				case targetClassName.search('odd')>-1:
-					location.href = '#';//product details
+				case 'search-form__button':
+					checkSearchField(e);
 				break;
-				case targetClassName.search('slider__button_left')>-1:
-					scrollSliderLeft(e.target);
-					break;
-				case targetClassName.search('slider__button_right')>-1:
-					scrollSliderRight(e.target);
-					break;
 				default:
-					console.log(e.target.className);
-					break;
+					//console.log(e.target.className);
+				break;
 			}
 		});
 	});
+
+	function checkSearchField(e){
+		let field = document.querySelector('.search-form__input').value;
+		if (field != "") {
+			location.href = 'category-all.html';
+		}else{
+			e.preventDefault();
+			document.querySelector('.search-form').style.display = 'none';
+			document.querySelector('.search-button').style.display = 'block';
+		}
+	}
 
 })();
--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,32 @@
+(function() {
+	document.addEventListener("DOMContentLoaded", function() {
+		document.body.addEventListener("click", function(e) {
+			switch (e.target.className) {
+				case 'slider__button_left':
+					scrollSliderLeft(e.target);
+					break;
+				case 'slider__button_right':
+					scrollSliderRight(e.target);
+					break;
+				default:
+					//console.log(e.target.className);
+				break;
+			}
+		});
+	});
+	
+	function scrollSliderLeft(e) {
+		let slider = document.getElementsByClassName('slider')[0];
+		slider.scrollLeft -= 100;
+		e.style.left = slider.scrollLeft + 'px';
+		document.getElementsByClassName('slider__button_right')[0].style.right = -slider.scrollLeft + 'px';
+	}
 
+	function scrollSliderRight(e) {
+		let slider = document.getElementsByClassName('slider')[0];
+		slider.scrollLeft += 100;
+		document.getElementsByClassName('slider__button_left')[0].style.left = slider.scrollLeft + 'px';
+		e.style.right = -slider.scrollLeft + 'px';
+	}
 
-function scrollSliderLeft(e) {
-	let slider = document.getElementsByClassName('slider')[0];
-	slider.scrollLeft -= 100;
-	e.style.left = slider.scrollLeft + 'px';
-	document.getElementsByClassName('slider__button_right')[0].style.right = -slider.scrollLeft + 'px';
-}
-
-function scrollSliderRight(e) {
-	let slider = document.getElementsByClassName('slider')[0];
-	slider.scrollLeft += 100;
-	document.getElementsByClassName('slider__button_left')[0].style.left = slider.scrollLeft + 'px';
-	e.style.right = -slider.scrollLeft + 'px';
-}
+})();
