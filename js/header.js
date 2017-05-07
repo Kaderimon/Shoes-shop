361342980c067b7cf08c8df4183935d33925fa6e
Kaderimon <schweppes2006@online.ua>
2017-05-07 05:49:26 +0300

    added responsive

--- js/header.js
+++ js/header.js
@@ -2,18 +2,25 @@
 
 	document.addEventListener("DOMContentLoaded", function() {
 		document.body.addEventListener("click", function(e) {
-			switch (e.target.className) {
-				case 'logo__text':
+			let targetClassName = e.target.className;
+			switch (true) {
+				case targetClassName.search('logo__text')>-1:
 					location.href = '#';
 					break;
-				case 'slider__button_left':
+				case targetClassName.search('even')>-1:
+					location.href = '#';//category listing
+				break;
+				case targetClassName.search('odd')>-1:
+					location.href = '#';//product details
+				break;
+				case targetClassName.search('slider__button_left')>-1:
 					scrollSliderLeft(e.target);
 					break;
-				case 'slider__button_right':
+				case targetClassName.search('slider__button_right')>-1:
 					scrollSliderRight(e.target);
 					break;
 				default:
-					//console.log(e.target.className);
+					console.log(e.target.className);
 					break;
 			}
 		});
