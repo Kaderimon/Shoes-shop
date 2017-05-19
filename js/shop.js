238ad05b93baf8009361568ad2b6afdc74a66da0
Kaderimon <schweppes2006@online.ua>
2017-05-19 21:41:36 +0300

    small changes

--- js/shop.js
+++ js/shop.js
@@ -2,6 +2,7 @@
 	document.addEventListener("DOMContentLoaded", function() {
 		loadItems();
 		setTotal();
+		disableOrder();
 		document.body.addEventListener("click", function(e) {
 			switch (e.target.className) {
 				case 'order-item__input':
@@ -13,6 +14,7 @@
 					break;
 				case 'order-item__del':
 					deleteItem(e);
+					disableOrder();
 					break;
 				default:
 					//console.log(e.target);
@@ -26,121 +28,146 @@
 			let bagWrap = document.getElementsByClassName("order")[0];
 			let cartData = JSON.parse(localStorage.getItem('cart'));
 			if (cartData) {
-				for(item in cartData){
+				for (item in cartData) {
 					if (item === e.target.parentNode.parentNode.className.split(" ")[1]) {
 						cartData[item][5] = Number(e.target.value);
 					};
 				}
 			}
-			if(!setDataToLS(cartData)){ // Обновляем данные в LocalStorage
-				bagWrap.innerHTML="";
+			if (!setDataToLS(cartData)) { // Обновляем данные в LocalStorage
+				bagWrap.innerHTML = "";
 				basketQty();
 				loadItems();
-				setTotal();		
+				setTotal();
 			}
 		} else {
-		    alert("Sorry, your browser does not support Web Storage...");
+			alert("Sorry, your browser does not support Web Storage...");
 		}
 	}
-	function deleteItem(e){
+
+	function disableOrder() {
+		if (localStorage !== undefined) {
+			let orderButton = document.querySelector(".buttons__order");
+			if (localStorage.cart === undefined) {
+				orderButton.disabled = true;
+				orderButton.style.opacity = "0.5";
+			} else {
+				if (localStorage.cart.length <= 2) {
+					orderButton.disabled = true;
+				orderButton.style.opacity = "0.5";
+				} else {
+					orderButton.disabled = false;
+					orderButton.style.opacity = "1";
+				}
+
+			}
+		} else {
+			alert("Sorry, your browser does not support Web Storage...");
+		}
+	}
+
+	function deleteItem(e) {
 		if (localStorage !== undefined) {
 			let bagWrap = document.getElementsByClassName("order")[0];
 			let cartData = JSON.parse(localStorage.getItem('cart'));
 			if (cartData) {
-				for(item in cartData){
+				for (item in cartData) {
 					if (item === e.target.parentNode.className.split(" ")[1]) {
-						if (cartData[item][5]===1) {
+						if (cartData[item][5] <= 1) {
 							delete cartData[item];
-						}else{
+						} else {
 							cartData[item][5] -= 1;
 						}
 					};
 				}
 			}
-			if(!setDataToLS(cartData)){ // Обновляем данные в LocalStorage
-				bagWrap.innerHTML="";
+			if (!setDataToLS(cartData)) { // Обновляем данные в LocalStorage
+				bagWrap.innerHTML = "";
 				basketQty();
 				loadItems();
-				setTotal();		
+				setTotal();
 			}
 		} else {
-		    alert("Sorry, your browser does not support Web Storage...");
+			alert("Sorry, your browser does not support Web Storage...");
 		}
 	}
 	//Считываем из LocalStorage
-	function getDataFromLS(){
-	  return JSON.parse(localStorage.getItem('cart'));
+	function getDataFromLS() {
+		return JSON.parse(localStorage.getItem('cart'));
 	}
 	// Записываем данные в LocalStorage
-	function setDataToLS(o){
-	  localStorage.setItem('cart', JSON.stringify(o));
-	  return false;
+	function setDataToLS(o) {
+		localStorage.setItem('cart', JSON.stringify(o));
+		return false;
 	}
 	//Выводит общую сумму на странице
-	function setTotal(){
+	function setTotal() {
 		if (localStorage !== undefined) {
 			let count = 0;
 			let totalPrice = 0;
 			let cartData = JSON.parse(localStorage.getItem('cart'));
 			let bagItem = document.getElementsByClassName("total__price")[0];
 			if (cartData) {
-				for(item in cartData){
+				for (item in cartData) {
 					count += cartData[item][5];
-					totalPrice += cartData[item][5]*cartData[item][1];
+					totalPrice += cartData[item][5] * cartData[item][1];
 				}
 			}
-			bagItem.innerHTML=totalPrice.toFixed(2)+"$";			
+			bagItem.innerHTML = totalPrice.toFixed(2) + "$";
 		} else {
-		    alert("Sorry, your browser does not support Web Storage...");
+			alert("Sorry, your browser does not support Web Storage...");
 		}
 	}
-	function itemsHead(){
-		let bagWrap = document.getElementsByClassName('order')[0];	
-		bagWrap.innerHTML += '<div class="order-head">'+
-				'<p class="order-item__prod">PRODUCT</p>'+
-				'<p class="order-item__desc">DESCRIPTION</p>'+
-				'<p class="order-item__color">COLOR</p>'+
-				'<p class="order-item__size">SIZE</p>'+
-				'<p class="order-item__qty">QTY</p>'+
-				'<p class=" order-item__price">AMOUNT</p>'+
-				'<p class="order-head__elem">DELETE</p></div>';
+
+	function itemsHead() {
+		let bagWrap = document.getElementsByClassName('order')[0];
+		bagWrap.innerHTML += '<div class="order-head">' +
+			'<p class="order-item__prod">PRODUCT</p>' +
+			'<p class="order-item__desc">DESCRIPTION</p>' +
+			'<p class="order-item__color">COLOR</p>' +
+			'<p class="order-item__size">SIZE</p>' +
+			'<p class="order-item__qty">QTY</p>' +
+			'<p class=" order-item__price">AMOUNT</p>' +
+			'<p class="order-head__elem">DELETE</p></div>';
 	}
-	function itemsFooter(){
-		let bagWrap = document.getElementsByClassName('order')[0];	
-		bagWrap.innerHTML += '<div class="total">'+
-				'<p class="total__text">Subtotal:</p>'+
-				'<p class="total__price">1000$</p></div>';
+
+	function itemsFooter() {
+		let bagWrap = document.getElementsByClassName('order')[0];
+		bagWrap.innerHTML += '<div class="total">' +
+			'<p class="total__text">Subtotal:</p>' +
+			'<p class="total__price">1000$</p></div>';
 	}
 	//Загрузка из корзины
 	function loadItems() {
 		let cart = getDataFromLS();
 		itemsHead();
-		if(cart){
-			for(item in cart){
-				fillPageFromCart(cart,item);
+		if (cart) {
+			for (item in cart) {
+				fillPageFromCart(cart, item);
 			}
 		}
 		itemsFooter();
 	}
 	//Отображение данных из корзины
-	function fillPageFromCart(cart,item){
+	function fillPageFromCart(cart, item) {
 		let data = cart[item];
-		let bagWrap = document.getElementsByClassName('order')[0];	
+		let bagWrap = document.getElementsByClassName('order')[0];
 		let closeDivTag = "</div>";
-		bagWrap.innerHTML += '<div class="order-item '+item+'">'+
-				'<div class="order-item__img '+item+'"></div>'+
-				'<div class="order-item__desc">'+
-				'<h2 class="order-item__name">'+data[0]+'</h2>'+
-				'<p class="order-item__art">Ref. 2514/302</p>'+closeDivTag+
-				'<p class="order-item__color">'+data[3]+'</p>'+
-				'<p class="order-item__size">'+data[2]+'</p>'+
-				'<div class="order-item__qty">'+
-				'<input type="number" pattern="[0-9]" min="0" class="order-item__input" value="'+data[5]+'"></div>'+
-				'<p class="order-item__price">'+data[1]+'</p>'+
-				'<button class="order-item__del">X</button>'+closeDivTag;
-		let itemImg = document.getElementsByClassName("order-item__img "+item)[0];
+		bagWrap.innerHTML += '<div class="order-item ' + item + '">' +
+			'<div class="order-item__img ' + item + '"></div>' +
+			'<div class="order-item__desc">' +
+			'<h2 class="order-item__name">' + data[0] + '</h2>' +
+			'<p class="order-item__art">Ref. 2514/302</p>' + closeDivTag +
+			'<p class="order-item__color">' + data[3] + '</p>' +
+			'<p class="order-item__size">' + data[2] + '</p>' +
+			'<div class="order-item__qty">' +
+			'<input type="number" pattern="[0-9]" min="0" class="order-item__input" value="' + data[5] + '"></div>' +
+			'<p class="order-item__price">' + data[1] + '</p>' +
+			'<button class="order-item__del">X</button>' + closeDivTag;
+		let itemImg = document.getElementsByClassName("order-item__img " + item)[0];
 		itemImg.style.background = data[4];
 		itemImg.style.backgroundPosition = "center";
 		itemImg.style.backgroundRepeat = "no-repeat";
+		itemImg.style.backgroundSize = "cover";
 	}
 })();
--- js/slider.js
+++ js/slider.js
@@ -1,30 +1,48 @@
 (function() {
+	let sliderScroll;
+	let slider = document.getElementsByClassName('slider')[0];
 	document.addEventListener("DOMContentLoaded", function() {
+		calcScroll()
 		document.body.addEventListener("click", function(e) {
 			switch (e.target.className) {
 				case 'slider__button_left':
 					scrollSliderLeft(e.target);
+					calcScroll();
 					break;
 				case 'slider__button_right':
 					scrollSliderRight(e.target);
+					calcScroll();
 					break;
 				default:
 					//console.log(e.target.className);
-				break;
+					break;
 			}
 		});
 	});
-	
+
+	function calcScroll() {
+		let buttonsWidth = 80;
+		let itemWidth = 200;
+		let sliderStyles = window.getComputedStyle(slider);
+		let sliderWidth = sliderStyles.width.split("");
+		sliderWidth.length = sliderWidth.length - 2;
+		sliderWidth = sliderWidth.join("");
+		let calcSroll = (sliderWidth - buttonsWidth) / itemWidth;
+		if (slider.scrollLeft == 0) {
+			sliderScroll = calcSroll / 10 * 200;
+		} else {
+			sliderScroll = 200;
+		}
+	}
+
 	function scrollSliderLeft(e) {
-		let slider = document.getElementsByClassName('slider')[0];
-		slider.scrollLeft -= 100;
+		slider.scrollLeft -= sliderScroll;
 		e.style.left = slider.scrollLeft + 'px';
 		document.getElementsByClassName('slider__button_right')[0].style.right = -slider.scrollLeft + 'px';
 	}
 
 	function scrollSliderRight(e) {
-		let slider = document.getElementsByClassName('slider')[0];
-		slider.scrollLeft += 100;
+		slider.scrollLeft += sliderScroll;
 		document.getElementsByClassName('slider__button_left')[0].style.left = slider.scrollLeft + 'px';
 		e.style.right = -slider.scrollLeft + 'px';
 	}
